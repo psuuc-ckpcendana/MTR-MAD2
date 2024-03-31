f0a21faa862bae2181c3b9c409230e17772d81ad
Christian Kaide Cendaña <132423063+psuuc-ckpcendana@users.noreply.github.com>
2024-04-01 01:02:38 +0800

    updated dependencies

--- lib/manananggal/game/configuration.dart
+++ lib/manananggal/game/configuration.dart
@@ -1,6 +1,6 @@
 class Config {
   static const groundHeight = 110.0;
-  static const gameSpeed = 180.0;
+  static const gameSpeed = 130.0;
   static const pipeInterval = 1.5;
   static const birdVelocity = 260;
   static const gravity = -75.0;
